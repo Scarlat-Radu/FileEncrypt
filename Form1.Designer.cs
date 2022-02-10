39d779ac476adcec231196928b6209d6a4c5ed5a
Radu <radu.scarlat@allengra.eu>
2022-02-10 10:44:32 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Cryptographic_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEncryptFile = new System.Windows.Forms.Button();
+            this.buttonDecryptFile = new System.Windows.Forms.Button();
+            this.buttonCreateAsmKeys = new System.Windows.Forms.Button();
+            this.buttonExportPublicKey = new System.Windows.Forms.Button();
+            this.buttonImportPublicKey = new System.Windows.Forms.Button();
+            this.buttonGetPrivateKey = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.encryptOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.decryptOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonEncryptFile
+            // 
+            this.buttonEncryptFile.Location = new System.Drawing.Point(3, 3);
+            this.buttonEncryptFile.Name = "buttonEncryptFile";
+            this.buttonEncryptFile.Size = new System.Drawing.Size(75, 40);
+            this.buttonEncryptFile.TabIndex = 0;
+            this.buttonEncryptFile.Text = "EncryptFile";
+            this.buttonEncryptFile.UseVisualStyleBackColor = true;
+            this.buttonEncryptFile.Click += new System.EventHandler(this.buttonEncryptFile_Click);
+            // 
+            // buttonDecryptFile
+            // 
+            this.buttonDecryptFile.Location = new System.Drawing.Point(84, 3);
+            this.buttonDecryptFile.Name = "buttonDecryptFile";
+            this.buttonDecryptFile.Size = new System.Drawing.Size(75, 40);
+            this.buttonDecryptFile.TabIndex = 1;
+            this.buttonDecryptFile.Text = "DecryptFile";
+            this.buttonDecryptFile.UseVisualStyleBackColor = true;
+            this.buttonDecryptFile.Click += new System.EventHandler(this.buttonDecryptFile_Click);
+            // 
+            // buttonCreateAsmKeys
+            // 
+            this.buttonCreateAsmKeys.Location = new System.Drawing.Point(165, 3);
+            this.buttonCreateAsmKeys.Name = "buttonCreateAsmKeys";
+            this.buttonCreateAsmKeys.Size = new System.Drawing.Size(90, 40);
+            this.buttonCreateAsmKeys.TabIndex = 2;
+            this.buttonCreateAsmKeys.Text = "Create Arms Key";
+            this.buttonCreateAsmKeys.UseVisualStyleBackColor = true;
+            this.buttonCreateAsmKeys.Click += new System.EventHandler(this.buttonCreateAsmKeys_Click);
+            // 
+            // buttonExportPublicKey
+            // 
+            this.buttonExportPublicKey.Location = new System.Drawing.Point(84, 49);
+            this.buttonExportPublicKey.Name = "buttonExportPublicKey";
+            this.buttonExportPublicKey.Size = new System.Drawing.Size(75, 40);
+            this.buttonExportPublicKey.TabIndex = 3;
+            this.buttonExportPublicKey.Text = "ExportPublicKey";
+            this.buttonExportPublicKey.UseVisualStyleBackColor = true;
+            this.buttonExportPublicKey.Click += new System.EventHandler(this.buttonExportPublicKey_Click);
+            // 
+            // buttonImportPublicKey
+            // 
+            this.buttonImportPublicKey.Location = new System.Drawing.Point(3, 49);
+            this.buttonImportPublicKey.Name = "buttonImportPublicKey";
+            this.buttonImportPublicKey.Size = new System.Drawing.Size(75, 40);
+            this.buttonImportPublicKey.TabIndex = 4;
+            this.buttonImportPublicKey.Text = "ImportPublicKey";
+            this.buttonImportPublicKey.UseVisualStyleBackColor = true;
+            this.buttonImportPublicKey.Click += new System.EventHandler(this.buttonImportPublicKey_Click);
+            // 
+            // buttonGetPrivateKey
+            // 
+            this.buttonGetPrivateKey.Location = new System.Drawing.Point(165, 49);
+            this.buttonGetPrivateKey.Name = "buttonGetPrivateKey";
+            this.buttonGetPrivateKey.Size = new System.Drawing.Size(90, 40);
+            this.buttonGetPrivateKey.TabIndex = 5;
+            this.buttonGetPrivateKey.Text = "GetPrivateKey";
+            this.buttonGetPrivateKey.UseVisualStyleBackColor = true;
+            this.buttonGetPrivateKey.Click += new System.EventHandler(this.buttonGetPrivateKey_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.buttonEncryptFile);
+            this.flowLayoutPanel1.Controls.Add(this.buttonDecryptFile);
+            this.flowLayoutPanel1.Controls.Add(this.buttonCreateAsmKeys);
+            this.flowLayoutPanel1.Controls.Add(this.buttonImportPublicKey);
+            this.flowLayoutPanel1.Controls.Add(this.buttonExportPublicKey);
+            this.flowLayoutPanel1.Controls.Add(this.buttonGetPrivateKey);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 54);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(289, 114);
+            this.flowLayoutPanel1.TabIndex = 8;
+            // 
+            // encryptOpenFileDialog
+            // 
+            this.encryptOpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // decryptOpenFileDialog
+            // 
+            this.decryptOpenFileDialog.FileName = "openFileDialog2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 176);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Encryption";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "KeyName: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 200);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEncryptFile;
+        private System.Windows.Forms.Button buttonDecryptFile;
+        private System.Windows.Forms.Button buttonCreateAsmKeys;
+        private System.Windows.Forms.Button buttonExportPublicKey;
+        private System.Windows.Forms.Button buttonImportPublicKey;
+        private System.Windows.Forms.Button buttonGetPrivateKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.OpenFileDialog encryptOpenFileDialog;
+        private System.Windows.Forms.OpenFileDialog decryptOpenFileDialog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
